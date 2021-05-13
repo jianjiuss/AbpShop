8f8225fc16983e419e33048aa043aa571e2992c1
jianjiuss <409424033@qq.com>
2021-05-13 16:54:50 +0800

    简化Domain项目

--- src/PZ.Shop.Domain/Data/IShopDbSchemaMigrator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace PZ.Shop.Data
-{
-    public interface IShopDbSchemaMigrator
-    {
-        Task MigrateAsync();
-    }
-}
--- src/PZ.Shop.Domain/ShopConsts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PZ.Shop
-{
-    public static class ShopConsts
-    {
-        public const string DbTablePrefix = "App";
-
-        public const string DbSchema = null;
-    }
-}
